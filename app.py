676380d3285453f1ebdf37ef96bfbe14d7b15ac4
Mymicc <aymericbesnard1604@gmail.com>
2025-10-21 09:32:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.header("mon application")
 
-nombre = [&, 2, 4, 7]
+nombre = [1, 2, 4, 7]
 carré = [1**2, 2**2, 4**2, 7**2]
 
 d = {"nombres": nombre, "carré": carré}
