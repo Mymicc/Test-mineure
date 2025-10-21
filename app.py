92c59847d37b8755a92d40fca588217f074dc267
Mymicc <aymericbesnard1604@gmail.com>
2025-10-21 09:31:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 st.header("mon application")
-st.write("Hamilton")
+
+nombre = [&, 2, 4, 7]
+carré = [1**2, 2**2, 4**2, 7**2]
+
+d = {"nombres": nombre, "carré": carré}
+data = pd.Dataframe(d)
+
+st.dataframe(data)
